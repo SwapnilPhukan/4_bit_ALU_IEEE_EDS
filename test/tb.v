bf1e8f3558bcc269b419da7fc6c86b8545d4f2a0
SWAPNIL PHUKAN <swapnilphukan1@gmail.com>
2026-09-15 20:00:43 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,23 +1,37 @@
 `default_nettype none
+`timescale 1ns / 1ps
 
 module tb;
 
+    // Tiny Tapeout control signals
+    reg clk;
+    reg rst_n;
+    reg ena;
+
     // Tiny Tapeout inputs
-    reg  [7:0] ui_in;
-    reg  [7:0] uio_in;
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
 
     // Tiny Tapeout outputs
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    // Tiny Tapeout control signals
-    reg  ena;
-    reg  clk;
-    reg  rst_n;
+`ifdef GL_TEST
+    // Gate-level power supplies
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
+`endif
 
     // Instantiate the ALU
     tt_um_combinational user_project (
+
+`ifdef GL_TEST
+        // Power pins required for gate-level simulation
+        .VPWR(VPWR),
+        .VGND(VGND),
+`endif
+
         .ui_in   (ui_in),
         .uo_out  (uo_out),
         .uio_in  (uio_in),
@@ -32,11 +46,12 @@ module tb;
         // Default values
         ui_in  = 8'b0;
         uio_in = 8'b0;
+
         ena    = 1'b1;
         clk    = 1'b0;
         rst_n  = 1'b1;
 
-        // Run simulation for a short time
+        // Keep simulation alive while Cocotb runs
         #10000;
 
         $finish;
